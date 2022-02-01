d9367515f4298bb92f2bd13b6c6e077c00391ac6
DangerousDaniel <danielcox996@gmail.com>
2022-02-01 16:35:28 -0400

    Basic Movement based off Vex Monsters

--- Arcade 2D Cooking Platformer/Assets/PlayerController2D.cs
+++ Arcade 2D Cooking Platformer/Assets/PlayerController2D.cs
@@ -3,31 +3,63 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
+    private float _moveHorizontal; 
+    [SerializeField] public float _moveSpeed; 
 
-    [SerializeField] public float MovementSpeed = 1f;
-    [SerializeField] public float JumpForce = 1f;
+    [SerializeField] private float _jumpHeight; 
+    [SerializeField] private bool isJumping;
 
-    private Rigidbody2D _rigidbody2d;
+    private Rigidbody2D _rigidbody2D;
 
-    private void Start()
+    
+    private void Awake() 
+    { 
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
-        _rigidbody2d = GetComponent<Rigidbody2D>();
+        Inputs();
     }
 
     private void FixedUpdate()
     {
-        //horizontal movement
-        var movement = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(movement, 0, 0) * MovementSpeed;
+        Move();
 
-        //flip code 
-        if (!Mathf.Approximately(0, movement))
-            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
-
-        //jump code
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody2d.velocity.y) < 0.001f)
+        //Jump
+        //Check if jump is occurring in Update()
+        if (isJumping)
         {
-            _rigidbody2d.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+             isJumping = false;
+
+            Jump();
         }
+    }
+
+    private void Inputs()
+    {
+        //Move the player left and right
+        _moveHorizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody2D.velocity.y) < 0.001f)
+        {
+            isJumping = true;
+        }
+    }
+
+    private void Move()
+    {
+        //Move the player
+        _rigidbody2D.velocity = new Vector2(_moveHorizontal * _moveSpeed, _rigidbody2D.velocity.y);
+    }
+
+    private void Jump()
+    {
+        //Set up the vertical velocity
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+
+        //Modify the velocity with force
+        _rigidbody2D.AddForce(Vector2.up * _jumpHeight * 2, ForceMode2D.Impulse);
+
     }
 }
