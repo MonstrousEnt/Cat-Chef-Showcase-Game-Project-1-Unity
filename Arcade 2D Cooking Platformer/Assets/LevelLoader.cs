1f45904fcecf98a7bdc6665a1ffe660ceaa17fba
DangerousDaniel <danielcox996@gmail.com>
2022-02-01 20:25:17 -0400

    Start Screen UI and Code Done

---
+++ Arcade 2D Cooking Platformer/Assets/LevelLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* Name: Daniel Cox, Ben Topple
+ * Date: December 8 - April 2, 2021
+ * Description: The class for loading the levels.
+ * Notes: 
+ * Level Load Tutorial Used: https://youtu.be/CE9VOZivb3I
+ */
+
+public class LevelLoader: MonoBehaviour
+{
+	//Class Variables
+	//[SerializeField] private Animator _animator; 
+	[SerializeField] private float _transtionTime = 1f; 
+
+	/// <summary>
+	/// Load the next scene.
+	/// </summary>
+	/// <param name="sceneName"></param>
+	public void LoadNextLevel(string sceneName)
+	{
+		LoadLevel(sceneName);
+	}
+
+	/// <summary>
+	/// Load the next scene, and run the cross fade transition before the next level load.
+	/// </summary>
+	/// <param name="sceneName"></param>
+	/// <returns></returns>
+	private void LoadLevel(string sceneName)
+	{
+		////Play the transition
+		//_animator.SetTrigger("StartCrossfade");
+
+		////Wait for a couple seconds
+		//yield return new WaitForSeconds(_transtionTime);
+
+		//Load the next scene
+		SceneManager.LoadScene(sceneName);
+	}
+}
