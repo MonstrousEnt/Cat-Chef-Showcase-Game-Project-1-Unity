db2b7c014c0cefa07648b564641c1e73cbf6a882
DangerousDaniel <danielcox996@gmail.com>
2022-02-08 18:04:16 -0400

    work on emney

--- Arcade 2D Cooking Platformer/Assets/Scripts/Player/PlayerBase.cs
+++ Arcade 2D Cooking Platformer/Assets/Scripts/Player/PlayerBase.cs
@@ -7,7 +7,7 @@ public class PlayerBase : MonoBehaviour
 {
     public static PlayerBase instance; //A static reference of the class
 
-    [SerializeField] private int health = 0;
+    [SerializeField] public int health = 0;
     [SerializeField] private int fullHeartNum = 10;
     [SerializeField] private int maxHealth;
     [SerializeField] private int maxHealthPowerUp = 100;
@@ -15,8 +15,6 @@ public class PlayerBase : MonoBehaviour
     [SerializeField] private HealthBar healthBar;
 
     public bool isRespawn = false; //A boolean flag for when the player is respawn at a checkpoint
-    private int playerLayer; //layer of the player
-    private int enemyLayer; //layer of the enemy
     [SerializeField] private Animator _animator; //Reference to the animator 
 
 
@@ -33,8 +31,7 @@ public class PlayerBase : MonoBehaviour
 
     private void Start()
     {
-        playerLayer = LayerMask.NameToLayer("Player");
-        enemyLayer = LayerMask.NameToLayer("Enemy");
+        
 
         //set the health to max health of the player
         health = maxHealth;
@@ -85,9 +82,6 @@ public class PlayerBase : MonoBehaviour
         //Display it in the UI
         healthBar.UpdateHealthBar(health, maxHealthPowerUp);
 
-        StopAllCoroutines();
-        StartCoroutine(DamageIndicator());
-
         //If the player dies
         if (health <= 0)
 		{
@@ -96,25 +90,7 @@ public class PlayerBase : MonoBehaviour
 		}
 	}
 
-    private IEnumerator DamageIndicator()
-    {
-        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, true);
-
-        //take damage frame
-        _animator.SetTrigger("takeDamage");
-
-        //Turn the player red
-        gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-
-        //Wait a second or 2
-        yield return new WaitForSeconds(1.0f);
-
-        //Turn the player back to normal
-        gameObject.GetComponent<SpriteRenderer>().color = Color.white;
-
-        Physics2D.IgnoreLayerCollision(playerLayer, enemyLayer, false);
-
-    }
+    
 
     private IEnumerator Die()
     {
